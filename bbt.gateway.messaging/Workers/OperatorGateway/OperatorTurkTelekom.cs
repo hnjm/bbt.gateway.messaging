e18db8021611c473d36623e442073629ac809bb4
Uğur Karataş <ukaratas@rocketmail.com>
2021-10-21 17:58:40 +0300

    Add files via upload

---
+++ bbt.gateway.messaging/Workers/OperatorGateway/OperatorTurkTelekom.cs
@@ -0,0 +1,17 @@
+﻿using bbt.gateway.messaging.Models;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace bbt.gateway.messaging.Workers.OperatorGateway
+{
+    public class OperatorTurkTelekom : IOperatorGateway
+    {
+        public void SendOtp(Phone phone, string content, ConcurrentBag<SendOtpResponseLog> responses)
+        {
+            System.Diagnostics.Debug.WriteLine("TT otp is send");
+        }
+    }
+}
---
+++ bbt.gateway.messaging/Workers/OtpSender.cs
@@ -0,0 +1,76 @@
+﻿using bbt.gateway.messaging.Models;
+using bbt.gateway.messaging.Workers.OperatorGateway;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace bbt.gateway.messaging.Workers
+{
+    public class OtpSender
+    {
+        SendMessageSmsRequest _data;
+
+        Type[] operators = new Type[] { typeof(OperatorTurkcell), typeof(OperatorVodafone), typeof(OperatorTurkTelekom) };
+
+        public OtpSender(SendMessageSmsRequest data)
+        {
+            _data = data;
+        }
+
+        public SendSmsResponse SendMessage()
+        {
+            using (var db = new DatabaseContext())
+            {
+                // Telefon bilgisi ve aktif olan tum kara liste kayitlari getirilir.
+                var phoneConfiguration = db.PhoneConfigurations.Where(i =>
+                    i.Phone.CountryCode == _data.Phone.CountryCode &&
+                    i.Phone.Prefix == _data.Phone.Prefix &&
+                    i.Phone.Number == _data.Phone.Number
+                    )
+                    .Include(c => c.BlacklistEntries)
+                    .FirstOrDefault();
+
+                if (phoneConfiguration == null)
+                {
+                    SendMessageToUnknown();
+
+                }
+                else {
+
+                    SendMessageToKnown(phoneConfiguration);
+                }
+
+                return null;
+            }
+        }
+
+
+        private SendSmsResponse SendMessageToUnknown()
+        {
+
+            ConcurrentBag<SendOtpResponseLog> responses = new ConcurrentBag<SendOtpResponseLog>();
+
+            Parallel.ForEach(operators, currentElement =>
+            {
+                IOperatorGateway gateway =  (IOperatorGateway)Activator.CreateInstance(currentElement);
+                gateway.SendOtp(_data.Phone, "test", responses);
+            });
+
+
+
+            return null; 
+
+        }
+
+        private SendSmsResponse SendMessageToKnown(PhoneConfiguration phoneConfiguration)
+        {
+            return null;
+        }
+
+
+    }
+}
