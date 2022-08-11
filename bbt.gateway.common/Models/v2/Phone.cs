ffe52f3363debd94d64d959e4787ab53cf112892
Sercan Yüksel <72672537+SercanYuksel1@users.noreply.github.com>
2022-08-11 18:51:46 +0300

    Update Phone.cs

--- bbt.gateway.common/Models/v2/Phone.cs
+++ bbt.gateway.common/Models/v2/Phone.cs
@@ -5,13 +5,10 @@ namespace bbt.gateway.common.Models.v2
     public class Phone
     {
         [Required(ErrorMessage = "This Field is Mandatory")]
-        [Range(1, 10000, ErrorMessage = "CountryCode field is in between 1 and 10000")]
         public int CountryCode { get; set; }
         [Required(ErrorMessage = "This Field is Mandatory")]
-        [Range(1, 10000, ErrorMessage = "CountryCode field is in between 1 and 10000")]
         public int Prefix { get; set; }
         [Required(ErrorMessage = "This Field is Mandatory")]
-        [Range(1, 9999999, ErrorMessage = "CountryCode field is in between 1 and 9999999")]
         public int Number { get; set; }
 
         public override string ToString()
