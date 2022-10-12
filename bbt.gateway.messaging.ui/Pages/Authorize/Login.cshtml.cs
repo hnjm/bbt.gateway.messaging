c17a599dc5367b662fd0ce88ed7391f289038f93
Taner Şimşek <TSimsek@burgan.com.tr>
2022-10-12 14:43:15 +0300

    Login Path Düzenlemesi

--- bbt.gateway.messaging.ui/Pages/Authorize/Login.cshtml.cs
+++ bbt.gateway.messaging.ui/Pages/Authorize/Login.cshtml.cs
@@ -4,22 +4,29 @@ using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.JSInterop;
 using Okta.AspNetCore;
+using Microsoft.Extensions.Configuration;
+using bbt.gateway.messaging.ui.Base;
+
 namespace bbt.gateway.messaging.ui.Pages.Authorize
 {
     public class LoginModel : PageModel
     {
         [Inject]
         bbt.gateway.messaging.ui.Data.HttpContextAccessor contextAccessor { get; set; }
+
         public async  Task OnGet(string redirectUri)
         {
-
-            string redirect ="http://"+ HttpContext.Request.Host.Value+"/callback";
-
+           IConfiguration config= FrameworkDependencyHelper.Instance.Get<IConfiguration>();
+          string path=  config.GetValue<string>("Base:path")+"/callback";
+            //string redirect ="http://"+ HttpContext.Request.Host.Value+"/callback";
+            //await JS.InvokeAsync<string>("console.log", redirect);
             var authenticationProperties = new LoginAuthenticationPropertiesBuilder()
-                .WithRedirectUri(redirect)
+                .WithRedirectUri(path)
                 .Build();
            await HttpContext.ChallengeAsync(Auth0Constants.AuthenticationScheme, authenticationProperties);
         }
+        
     }
 }
